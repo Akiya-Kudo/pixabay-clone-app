4cf84945de1a37a6aba31187f8fbed56d0d5d142
Akiya-Kudo <k.akiya07@gmail.com>
2022-08-31 20:02:40 +0900

    1

--- src/ImageGallery.jsx
+++ src/ImageGallery.jsx
@@ -4,7 +4,7 @@ import './ImagesGallery.css';
 const ImageGallery = () => {
   return (
     <div>
-      <div className="images-wrapper">wrapper</div>
+      <div className="images-wrapper">feuture 1</div>
     </div>
   )
 }
