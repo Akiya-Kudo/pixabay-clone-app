e3357d4ac8b653acbd50702fcea04b037902cd97
Akiya-Kudo <k.akiya07@gmail.com>
2022-08-31 19:43:30 +0900

    first commit

---
+++ src/ImageGallery.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './ImagesGallery.css';
+
+const ImageGallery = () => {
+  return (
+    <div>
+      <div className="images-wrapper"></div>
+    </div>
+  )
+}
+
+export default ImageGallery
